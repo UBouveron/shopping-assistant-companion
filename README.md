89ae20fad2006061ffad21d9bfba7557217b9392
Ugo Bouveron <ugobouveron@gmail.com>
2019-03-06 18:01:30 +0100

    Without React
    Remaster Edition.

--- README.md
+++
@@ -1,2 +0,0 @@
-# shopping-assistant-companion
-React based website for the Shopping Assistant Android app.
--- config/paths.js
+++ config/paths.js
@@ -1,19 +1,12 @@
-// Paths (aliases) will export some path variables that we'll
-// use in other Webpack config and server files
-
-const path = require("path");
-const fs = require("fs");
-
-const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const path = require('path');
 
 module.exports = {
-  @assets: resolveApp("src/assets"), // For images and other assets
-  @build: resolveApp("build"), // Prod built files end up here
-  @config: resolveApp("config"), // App config files
-  @html: resolveApp("public/index.html"),
-  @indexjs: resolveApp("src/index.jsx"), // Main entry point
-  @src: resolveApp("src"), // App source
-  @font: resolveApp("src/assets/font"),
-  @img: resolveApp("src/assets/img")
+  ROOT: path.resolve(__dirname, '../'),
+  SRC: path.resolve(__dirname, '../', 'src'),
+  DIST: path.resolve(__dirname, '../', 'dist'),
+  APP: path.resolve(__dirname, '../', 'src', 'app'),
+  PUBLIC: path.resolve(__dirname, '../', 'src', 'public'),
+  STYLES: path.resolve(__dirname, '../', 'src', 'styles'),
+  IMAGES: path.resolve(__dirname, '../', 'src', 'public', 'assets', 'images'),
+  SHADERS: path.resolve(__dirname, '../', 'src', 'public', 'shaders')
 };
