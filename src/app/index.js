5a628a52a3e07b7224e616d51dd0bb3e355f6244
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-03-15 17:52:16 +0100

    feat: added features slide+fade anim on scroll

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,14 @@
 import SmoothScroll from 'smooth-scroll';
+import AOS from 'aos';
 
+import 'aos/dist/aos.css';
 import '@sass/main.scss';
 
 import '@images/iPhoneX.svg';
 
 new SmoothScroll('a[href*="#"]', {
-  speed: 750
+  speed: 650,
+  easing: 'easeOutQuad',
 });
+
+AOS.init();
