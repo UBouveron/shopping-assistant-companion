f154701dce90fc2235ee29d74b1b3ffad34d444a
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2019-03-21 21:57:57 +0100

    feat: wip temp loading transition

--- src/app/index.js
+++ src/app/index.js
@@ -2,26 +2,36 @@ import SmoothScroll from 'smooth-scroll';
 import AOS from 'aos';
 import lottie from 'lottie-web';
 
+import butlerJson from './butler.json';
+
 import 'aos/dist/aos.css';
 import '@sass/main.scss';
 
 import '@images/iPhoneX.svg';
 
-import butlerJson from './butler.json';
-
-console.log(butlerJson);
-
+// smooth scroll on click
 new SmoothScroll('a[href*="#"]', {
   speed: 650,
   easing: 'easeOutQuad',
 });
 
+// scroll animations
 AOS.init();
 
-const animation = lottie.loadAnimation({
+// intro character animation
+lottie.loadAnimation({
   container: document.getElementById('illustration'),
   rendrer: 'svg',
   loop: true,
   autoplay: true,
   animationData: butlerJson
+});
+
+// loader
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.style.display = 'initial';
+
+  // const loader = document.getElementById('loader');
+  // loader.remove();
 });
